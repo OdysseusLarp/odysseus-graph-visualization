6f3ba08708fa963b99c850ee2feed434721ad1d8
Santtu Pajukanta <santtu@pajukanta.fi>
2022-08-18 14:37:51 +0300

    no need to hardcode the amount of rows, can use Taulukko1!A:E without row numbers

--- odysseus_graph_visualization/sheets.py
+++ odysseus_graph_visualization/sheets.py
@@ -1,5 +1,6 @@
-from functools import cache
+import logging
 import os.path
+from functools import cache
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -10,7 +11,9 @@ from googleapiclient.discovery import build
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "1_MgBZXhKJ-HT6rUY2TCXWP5FW2TBGtnNGMFpR7RnaM4"
+ORG_DATA_SPREADSHEET_ID = "1_MgBZXhKJ-HT6rUY2TCXWP5FW2TBGtnNGMFpR7RnaM4"
+
+logger = logging.getLogger(__name__)
 
 
 def get_credentials():
@@ -28,19 +31,25 @@ def get_credentials():
     return creds
 
 
-@cache
-def get_organization_data(
-    spreadsheet_id=SAMPLE_SPREADSHEET_ID,
-    range="Taulukko1!A1:E449",  # TODO can the exact range be left unspecified?
-):
+def get_values(spreadsheet_id, range):
     creds = get_credentials()
     service = build("sheets", "v4", credentials=creds)
 
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range).execute()
 
-    values = result["values"]
+    return result["values"]
+
+
+@cache
+def get_organization_data(
+    spreadsheet_id=ORG_DATA_SPREADSHEET_ID,
+    range="Taulukko1!A:E",
+):
+    values = get_values(spreadsheet_id, range)
     header_row = values.pop(0)
+
+    logger.info("Found %s edges", len(values))
 
     return [dict(zip(header_row, row)) for row in values]
 
